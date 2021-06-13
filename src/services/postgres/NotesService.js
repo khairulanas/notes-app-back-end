7fd9d9bc29013f490dda3dcb058545530be7858f
khairul anas <mafiakanan@gmail.com>
2021-06-13 11:41:09 +0700

    postgress addnote service

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -1,7 +1,29 @@
+const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
 
 class NotesService {
   constructor() {
     this._pool = new Pool();
   }
+
+  async addNote({ title, body, tags }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const query = {
+      text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+      values: [id, title, body, tags, createdAt, updatedAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Catatan gagal ditambahkan');
+    }
+    return result.rows[0].id;
+  }
 }
+
+module.exports = NotesService;
