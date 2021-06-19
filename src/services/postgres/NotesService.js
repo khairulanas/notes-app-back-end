023b4efd1be0b2e44ee0a9c81c5b2aabadd6ac1a
khairul anas <mafiakanan@gmail.com>
2021-06-19 09:36:10 +0700

    note service: editNoteById+deleteNoteById

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -2,6 +2,7 @@ const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
 const { mapDBToModel } = require('../../utils');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class NotesService {
   constructor() {
@@ -40,6 +41,32 @@ class NotesService {
 
     const result = await this._pool.query(query);
     return result.rows.map(mapDBToModel)[0];
+  }
+
+  async editNoteById(id, { title, body, tags }) {
+    const updatedAt = new Date().toISOString();
+    const query = {
+      text: 'UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id',
+      values: [title, body, tags, updatedAt, id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');
+    }
+  }
+
+  async deleteNoteById(id) {
+    const query = {
+      text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
+    }
   }
 }
 
